d1e8f93494dda0898c56934c61fa5c873593e3b5
narendra <narendrapatidar842@gmail.com>
2023-01-09 11:23:21 +0530

    narendra

---
+++ projects/JS/add/script.js
@@ -0,0 +1,15 @@
+// function hello() {
+//     console.log("hello narendra")
+// }
+// function hi() {
+//     console.log("hii")
+// }
+
+function color() {
+    const a = document.getElementById("narendra")
+    a.style.backgroundColor = "red"
+}
+function colors() {
+    const b = document.getElementById("narendra")
+    b.style.backgroundColor = "yellow"
+}
